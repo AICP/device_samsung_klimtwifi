c2d79ce865a7ec0f4ead8b6c853a2ba60223a2c4
doc <doc.divxm@gmail.com>
2015-06-08 20:28:15 +0300

    klimtwifi: Switch to AICP
    Change-Id: I933e568521daafd1274e189a6013f10fa866bc15

--- device.mk
+++ device.mk
@@ -38,10 +38,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_effects.conf:system/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2560
-TARGET_SCREEN_WIDTH := 1600
-
 # Camera
 PRODUCT_PACKAGES += \
     camera.universal5420 \
