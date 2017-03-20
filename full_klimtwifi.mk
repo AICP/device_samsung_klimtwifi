a10edbf9a48ed89d24a20236788f11ed54388b9c
DarkExistence <darraghmcgee20@gmail.com>
2017-03-20 14:24:15 +0000

    AICPify

---
+++ full_klimtwifi.mk
@@ -0,0 +1,24 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common_tablet.mk)
+
+# Inherit from chagallwifi device
+$(call inherit-product, device/samsung/klimtwifi/device.mk)
+
+PRODUCT_NAME := aicp_klimtwifi
+PRODUCT_DEVICE := klimtwifi
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-T700
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_MODEL=SM-T700 \
+    PRODUCT_NAME=klimtwifi \
+    PRODUCT_DEVICE=klimtwifi \
+    TARGET_DEVICE=klimtwifi
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Sean Hoyt (deadman96385),Darragh McGee (DarkExistence)"
